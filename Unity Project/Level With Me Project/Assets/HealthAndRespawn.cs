9ffac113a7ce916aa0260e15948e5f619c022268
DR-DRU <negoescu@gmx.de>
2020-04-10 17:19:06 +0200

    Basic Health & Respawn

---
+++ Unity Project/Level With Me Project/Assets/HealthAndRespawn.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthAndRespawn : MonoBehaviour
+{
+    [SerializeField] private int maxHealth;
+    public Vector3 respawnPoint;
+    private int currentHealth;
+    [SerializeField] private float minimumHeight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckForRespawn();
+    }
+
+    void CheckForOutOfBounds()
+    {
+        if (transform.position.y < minimumHeight)
+        {
+            Respawn();
+        }
+    }
+
+    void CheckForRespawn()
+    {
+        if (currentHealth <= 0)
+        {
+            Respawn();
+        }
+    }
+
+    void Respawn()
+    {
+
+        transform.position = respawnPoint;
+        currentHealth = maxHealth;
+    }
+}
