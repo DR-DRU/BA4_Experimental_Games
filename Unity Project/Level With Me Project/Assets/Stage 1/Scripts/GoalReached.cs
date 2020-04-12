52ef53acab2780f3e9f0667f80292a60272fe8ab
DR-DRU <negoescu@gmx.de>
2020-04-12 16:59:49 +0200

    Folder Structure

---
+++ Unity Project/Level With Me Project/Assets/Stage 1/Scripts/GoalReached.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GoalReached : MonoBehaviour
+{
+    public float fadeoutFactor;
+    public Image blackScreen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.name == "Goal")
+        {
+            StartCoroutine("FadeOutFunction");
+        }
+    }
+
+    void LevelTransition()
+    {
+        //Load next level
+    }
+
+    IEnumerator FadeOutFunction()
+    {
+        while (blackScreen.color.a < 1)
+        {
+            Color c = blackScreen.color;
+            c.a += (Time.deltaTime / fadeoutFactor);
+            blackScreen.color = c;
+            yield return null;
+        }
+        LevelTransition();
+    }
+
+
+}
---
+++ Unity Project/Level With Me Project/Assets/Stage 1/Scripts/Projectile.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float projectileSpeed;
+    public int damage;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 5.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.forward * Time.deltaTime * projectileSpeed;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(3);
+        if(collision.gameObject.GetComponent<Enemy>() != null)
+        {
+            Debug.Log(4);
+            collision.gameObject.GetComponent<Enemy>().health -= damage;
+        }
+        else if (collision.gameObject.GetComponent<HealthAndRespawn>() != null)
+        {
+            Debug.Log(5);
+            collision.gameObject.GetComponent<HealthAndRespawn>().currentHealth -= damage;
+        }
+        Destroy(gameObject);
+    }
+}
